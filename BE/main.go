707d11328c2a37e090f31965f419a71d543ddada
hann <nguyenngocha100602@gmail.com>
2024-10-18 09:08:18 +0700

    update

--- BE/main.go
+++ BE/main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type TodoItem struct {
+	Id          int        `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Status      string     `json:"status,omitempty"`
+	CreatedAt   *time.Time `json:"created_at"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
+}
 
 func main() {
-    fmt.Println("alo")
+	now := time.Now().UTC()
+
+	item := TodoItem{
+		Id:          1,
+		Title:       "This is item 1",
+		Description: "This is item 1",
+		Status:      "This is item 1",
+		CreatedAt:   &now,
+		UpdatedAt:   nil,
+	}
+
+	jsonData, err := json.Marshal(item)
+
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	stringData := string(jsonData)
+
+	fmt.Println(stringData)
+
+	var item2 TodoItem
+
+	if err := json.Unmarshal([]byte(stringData), &item2); err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	fmt.Println(item2.Status)
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"Message": "pong",
+		})
+	})
+
+	r.Run()
 }
