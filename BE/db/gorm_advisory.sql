e92cf01c8cf1f3e720ddbd97c2cc2447bd6e95b3
hann <nguyenngocha100602@gmail.com>
2025-01-06 00:50:30 +0700

    fix: update sql query

--- BE/db/gorm_advisory.sql
+++ BE/db/gorm_advisory.sql
@@ -16,38 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `user`
+-- Table structure for table `advisory`
 --
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `advisory`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
+CREATE TABLE `advisory` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `created_at` datetime(3) DEFAULT NULL,
   `updated_at` datetime(3) DEFAULT NULL,
-  `full_name` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `phone_number` varchar(50) NOT NULL,
-  `status` varchar(10) NOT NULL,
-  `role` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uni_user_username` (`username`),
-  UNIQUE KEY `uni_user_phone_number` (`phone_number`),
-  CONSTRAINT `chk_user_password` CHECK ((char_length(`password`) >= 6)),
-  CONSTRAINT `chk_user_username` CHECK ((char_length(`username`) >= 6))
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `full_name` longtext NOT NULL,
+  `note` longtext NOT NULL,
+  `phone_number` longtext NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `advisory`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'2024-12-28 08:38:07.599',NULL,'Nguyen Ngoc Ha','admin1','admin1','0988464108','Active','Admin'),(4,'2025-01-04 08:56:12.291',NULL,'Nguyễn Ngọc Hà','user123','password1','0988464107','Active','User'),(10,'2025-01-05 16:15:22.241',NULL,'Hà Nguyễn','user12','password123123','0988464104','Active','User'),(12,'2025-01-05 16:16:30.623',NULL,'Hà Nguyễn','user124','password123','0988464109','Active','User');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `advisory` WRITE;
+/*!40000 ALTER TABLE `advisory` DISABLE KEYS */;
+INSERT INTO `advisory` VALUES (9,'2025-01-05 16:08:09.754',NULL,'Hà Nguyễn','rthrth','0988464108','nguyenngocha100602@gmail.com');
+/*!40000 ALTER TABLE `advisory` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- BE/db/gorm_order.sql
+++ BE/db/gorm_order.sql
@@ -26,10 +26,8 @@ CREATE TABLE `order` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `created_at` datetime(3) DEFAULT NULL,
   `updated_at` datetime(3) DEFAULT NULL,
-  `total_price` smallint NOT NULL,
+  `total_price` bigint NOT NULL,
   `delivery_date` datetime(3) DEFAULT NULL,
-  `latitude` decimal(19,17) NOT NULL,
-  `longitude` decimal(20,17) NOT NULL,
   `current_position` tinyint NOT NULL,
   `detail_address` varchar(255) NOT NULL,
   `note` varchar(255) NOT NULL,
@@ -43,13 +41,13 @@ CREATE TABLE `order` (
   `end_trailer_id` bigint DEFAULT NULL,
   `container_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`),
+  KEY `fk_dump_container_dump` (`container_id`),
+  KEY `fk_dump_customer_warehouse` (`customer_warehouse_id`),
+  KEY `fk_truck_port_dump` (`truck_id`),
   KEY `fk_user_orders` (`user_id`),
   KEY `fk_dump_port_dump` (`port_id`),
   KEY `fk_dump_start_trailer_dump` (`start_trailer_id`),
   KEY `fk_dump_end_trailer_dump` (`end_trailer_id`),
-  KEY `fk_dump_container_dump` (`container_id`),
-  KEY `fk_dump_customer_warehouse` (`customer_warehouse_id`),
-  KEY `fk_truck_port_dump` (`truck_id`),
   CONSTRAINT `fk_dump_container_dump` FOREIGN KEY (`container_id`) REFERENCES `dump` (`id`),
   CONSTRAINT `fk_dump_customer_warehouse` FOREIGN KEY (`customer_warehouse_id`) REFERENCES `dump` (`id`),
   CONSTRAINT `fk_dump_end_trailer_dump` FOREIGN KEY (`end_trailer_id`) REFERENCES `dump` (`id`),
@@ -57,7 +55,7 @@ CREATE TABLE `order` (
   CONSTRAINT `fk_dump_start_trailer_dump` FOREIGN KEY (`start_trailer_id`) REFERENCES `dump` (`id`),
   CONSTRAINT `fk_truck_port_dump` FOREIGN KEY (`truck_id`) REFERENCES `truck` (`id`),
   CONSTRAINT `fk_user_orders` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=144 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,6 +64,7 @@ CREATE TABLE `order` (
 
 LOCK TABLES `order` WRITE;
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
+INSERT INTO `order` VALUES (136,'2025-02-15 05:55:25.328','2025-02-15 07:55:11.877',311282,'2025-01-10 00:00:00.000',0,'d','f','Pending','IE',NULL,1,NULL,46,15,18,32),(137,'2025-02-15 06:46:38.789','2025-02-15 07:55:11.893',721140,'2025-01-11 00:00:00.000',0,'d','f','Pending','IE',NULL,1,NULL,45,15,23,37),(138,'2025-02-15 06:49:45.261','2025-02-15 07:55:11.898',694489,'2025-01-10 00:00:00.000',0,'b','b','Pending','IE',NULL,1,NULL,46,15,25,39),(139,'2025-02-15 06:53:11.840','2025-02-15 07:55:11.902',689986,'2025-01-09 00:00:00.000',0,'g','r','Pending','IE',NULL,1,NULL,47,15,23,37),(140,'2025-02-15 07:17:16.694','2025-02-15 07:55:11.905',689986,'2025-01-09 00:00:00.000',0,'g','r','Pending','IE',NULL,1,NULL,47,15,23,37),(141,'2025-02-15 07:56:55.714',NULL,689986,'2025-01-09 00:00:00.000',0,'g','r','Pending','IE',NULL,1,NULL,47,15,23,37),(142,'2025-02-15 07:56:57.590',NULL,689986,'2025-01-09 00:00:00.000',0,'g','r','Pending','IE',NULL,1,NULL,47,15,23,37),(143,'2025-02-15 07:56:57.640',NULL,689986,'2025-01-09 00:00:00.000',0,'g','r','Pending','IE',NULL,1,NULL,47,15,23,37);
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -78,4 +77,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-22 15:37:19
+-- Dump completed on 2025-01-06  0:37:41
